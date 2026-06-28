c4d5d9756ea3e5e9be71e2e56f7fe68c11e2ec3c
Accueil ICP EIADE <sabah6@hotmail.fr>
2026-06-28 09:00:02 +0200

    Add video data for cardiology topics

---
+++ videos.js
@@ -0,0 +1,35 @@
+const videos = [
+
+{
+    titre: "TAVI",
+    fichier: "videos/cardiologie/TAVI.mp4",
+    miniature: "miniatures/cardiologie/tavi.jpg",
+    motsCles: [
+        "tavi",
+        "valve",
+        "aortique",
+        "cardiologie",
+        "interventionnelle",
+        "coronaire",
+        "sapien",
+        "valvulopathie"
+    ]
+},
+
+{
+    titre: "Ablation de FA par cathéter",
+    fichier: "videos/cardiologie/FA_ablation_catheter.mp4",
+    miniature: "miniatures/cardiologie/fa.jpg",
+    motsCles: [
+        "fa",
+        "fibrillation",
+        "atriale",
+        "ablation",
+        "catheter",
+        "electrophysiologie",
+        "arythmie",
+        "cardiologie"
+    ]
+}
+
+];
