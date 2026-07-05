fe216f6902839a6f9076b9f3efc08fbeb4dd6a86
Accueil ICP EIADE <sabah6@hotmail.fr>
2026-07-05 19:33:45 +0200

    Update videos.js

--- videos.js
+++ videos.js
@@ -30,6 +30,21 @@ const videos = [
         "valvulopathie"
     ]
 },
+        {
+    titre: "VALVE TRICUSPIDE",
+    fichier: "videos/cardiologie/valvetricuspide.mp4",
+    miniature: "miniatures/cardiologie/tricuspide.jpg",
+    motsCles: [
+        "ttvr",
+        "valve",
+        "tricuspide",
+        "cardiologie",
+        "interventionnelle",
+        "insuffisance",
+        "edwards",
+        "valvulopathie"
+    ]
+},
 
 {
     titre: "Ablation de FA par radiofréquence",
